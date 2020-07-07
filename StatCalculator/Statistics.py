387f5318634c936acf3e9cbbf708f9e374562b2b
kb8njit <kb8@njit.edu>
2020-07-06 21:16:02 -0400

    Feature: Added stat functions

--- StatCalculator/Statistics.py
+++ StatCalculator/Statistics.py
@@ -1,4 +1,10 @@
 from Calculator.Calculator import Calculator
+from StatCalculator.Mean import mean
+from StatCalculator.Median import median
+from StatCalculator.Mode import mode
+from StatCalculator.Variance import variance
+from StatCalculator.StandardDev import stand_dev
+from StatCalculator.ZScore import z_score
 
 
 class Statistics(Calculator):
@@ -6,3 +12,29 @@ class Statistics(Calculator):
 
     def __init__(self):
         super().__init__()
+
+    def mean(self, data):
+        self.result = mean(data)
+        return self.result
+
+    def median(self, data):
+        self.result = median(data)
+        return self.result
+
+    def mode(self, data):
+        self.result = mode(data)
+        return self.result
+
+    def variance(self, data):
+        self.result = variance(data)
+        return self.result
+
+    def stand_dev(self, data):
+        self.result = stand_dev(data)
+        return self.result
+
+    def z_score(self, data):
+        self.result = z_score(data)
+        return self.result
+
+    pass
